f4c6aa00a580358fc9fddd555f38e8f8b850ed1a
Juan Calzoncit <_juan_alejandro@outlook.com>
2025-06-03 18:51:03 -0600

    Add: Creación de interfaze Icarrera

---
+++ curso_01/ICarrera.vb
@@ -0,0 +1,9 @@
+﻿Public Interface ICarrera
+
+    Sub Insert(ByVal nombre As String, ByVal siglas As String)
+
+    Sub DeleteById(ByVal carreraid As Integer)
+
+    Sub Update(ByVal carreraid As Integer, ByVal nombre As String, ByVal siglas As String)
+
+End Interface
