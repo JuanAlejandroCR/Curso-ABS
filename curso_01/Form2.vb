121a9a19ae1947cc76b382d9cc93e0c774bac272
Juan Calzoncit <_juan_alejandro@outlook.com>
2025-06-03 18:51:50 -0600

    Update: Uso de la clase DB.vb para las operación CRUD de la trabla alumno

--- curso_01/Form2.vb
+++ curso_01/Form2.vb
@@ -4,170 +4,58 @@ Imports System.Data.SqlClient
 
 Public Class Form2
 
-    Dim conexion As SqlConnection
-
     Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles MyBase.Load
-
-        ' Lee el string de conexion a la DB en el archivo config\conexion.txt
-        Dim rutaBase As String = Path.GetFullPath(Path.Combine(Application.StartupPath, "..\..\"))
-        Dim rutaConexion As String = Path.Combine(rutaBase, "config\conexion.txt")
-
-        Dim cadenaConexion As String = File.ReadAllText(rutaConexion)
-
-        conexion = New SqlConnection()
-        conexion.ConnectionString = cadenaConexion
-        conexion.Open()
-
         llenaGrid()
     End Sub
 
     Private Sub llenaGrid()
-        Dim comando As SqlCommand
-        comando = New SqlCommand
-        comando.Connection = conexion
-        comando.CommandText = "spAlumnoGetList"
-        comando.CommandType = CommandType.StoredProcedure
+        Dim db As DB
+        db = New DB()
 
-        Dim resultados As DataSet
-        resultados = New DataSet()
-
-        Dim adaptador As SqlDataAdapter
-        adaptador = New SqlDataAdapter()
-        adaptador.SelectCommand = comando
-        adaptador.Fill(resultados, "alumnos")
-
-        dgvAlumnos.DataSource = resultados.Tables("alumnos")        
+        dgvAlumnos.DataSource = db.ExecuteResultSet("spAlumnoGetList")
     End Sub
 
 
     Private Sub btnAgregar_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnAgregar.Click
 
-        Dim comando As SqlCommand
-        comando = New SqlCommand
-        comando.Connection = conexion
-        comando.CommandText = "spAlumnoInsert"
-        comando.CommandType = CommandType.StoredProcedure
+        Dim db As DB
+        db = New DB()
+        db.AddParemeter("nombre", txtNombre.Text)
+        db.AddParemeter("matricula", txtMatricula.Text)
+        db.AddParemeter("apellidopaterno", txtApellidoPaterno.Text)
+        db.AddParemeter("apellidomaterno", txtApellidoMaterno.Text)
+        db.AddParemeter("carreraid", txtCarreraId.Text)
 
-        Dim parNombre As SqlParameter
-        parNombre = New SqlParameter
-        parNombre.ParameterName = "nombre"
-        parNombre.Value = txtNombre.Text
+        db.AddOutputParameter("alumnoid", 0, 0)
+        db.Execute("spAlumnoInsert")
 
-        comando.Parameters.Add(parNombre)
-
-        Dim parMatricula As SqlParameter
-        parMatricula = New SqlParameter
-        parMatricula.ParameterName = "matricula"
-        parMatricula.Value = txtMatricula.Text
-
-        comando.Parameters.Add(parMatricula)
-
-        Dim parApellidoPaterno As SqlParameter
-        parApellidoPaterno = New SqlParameter
-        parApellidoPaterno.ParameterName = "apellidopaterno"
-        parApellidoPaterno.Value = txtApellidoPaterno.Text
-
-        comando.Parameters.Add(parApellidoPaterno)
-
-        Dim parApellidoMaterno As SqlParameter
-        parApellidoMaterno = New SqlParameter
-        parApellidoMaterno.ParameterName = "apellidomaterno"
-        parApellidoMaterno.Value = txtApellidoMaterno.Text
-
-        comando.Parameters.Add(parApellidoMaterno)
-
-        Dim parCarreraId As SqlParameter
-        parCarreraId = New SqlParameter
-        parCarreraId.ParameterName = "carreraid"
-        parCarreraId.Value = txtCarreraId.Text
-
-        comando.Parameters.Add(parCarreraId)
-
-        Dim parAlumnoId As SqlParameter
-        parAlumnoId = New SqlParameter
-        parAlumnoId.ParameterName = "alumnoid"
-        parAlumnoId.Value = 0
-        parAlumnoId.Direction = ParameterDirection.Output
-
-        comando.Parameters.Add(parAlumnoId)
-
-
-        comando.ExecuteNonQuery()
-
-        txtAlumnoId.Text = comando.Parameters("alumnoid").Value
+        txtAlumnoId.Text = db.OutParameter("alumnoid")
 
         llenaGrid()
     End Sub
 
     Private Sub btnEliminar_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnEliminar.Click
-        Dim comando As SqlCommand
-        comando = New SqlCommand
-        comando.Connection = conexion
-        comando.CommandText = "spAlumnoDeleteById"
-        comando.CommandType = CommandType.StoredProcedure
 
-        Dim parAlumnoId As SqlParameter
-        parAlumnoId = New SqlParameter
-        parAlumnoId.ParameterName = "alumnoid"
-        parAlumnoId.Value = CInt(txtAlumnoId.Text)
+        Dim db As DB
+        db = New DB()
+        db.AddParemeter("alumnoid", CInt(txtAlumnoId.Text))
+        db.Execute("spAlumnoDeleteById")
 
-        comando.Parameters.Add(parAlumnoId)
-
-        comando.ExecuteNonQuery()
         llenaGrid()
     End Sub
 
     Private Sub btnActualizar_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnActualizar.Click
-        Dim comando As SqlCommand
-        comando = New SqlCommand
-        comando.Connection = conexion
-        comando.CommandText = "spAlumnoUpdate"
-        comando.CommandType = CommandType.StoredProcedure
 
+        Dim db As DB
+        db = New DB()
+        db.AddParemeter("alumnoid", CInt(txtAlumnoId.Text))
+        db.AddParemeter("nombre", txtNombre.Text)
+        db.AddParemeter("matricula", txtMatricula.Text)
+        db.AddParemeter("apellidopaterno", txtApellidoPaterno.Text)
+        db.AddParemeter("apellidomaterno", txtApellidoMaterno.Text)
+        db.AddParemeter("carreraid", txtCarreraId.Text)
 
-        Dim parAlumnoId As SqlParameter
-        parAlumnoId = New SqlParameter
-        parAlumnoId.ParameterName = "alumnoid"
-        parAlumnoId.Value = CInt(txtAlumnoId.Text)
-
-        comando.Parameters.Add(parAlumnoId)
-
-        Dim parNombre As SqlParameter
-        parNombre = New SqlParameter
-        parNombre.ParameterName = "nombre"
-        parNombre.Value = txtNombre.Text
-
-        comando.Parameters.Add(parNombre)
-
-        Dim parMatricula As SqlParameter
-        parMatricula = New SqlParameter
-        parMatricula.ParameterName = "matricula"
-        parMatricula.Value = txtMatricula.Text
-
-        comando.Parameters.Add(parMatricula)
-
-        Dim parApellidoPaterno As SqlParameter
-        parApellidoPaterno = New SqlParameter
-        parApellidoPaterno.ParameterName = "apellidopaterno"
-        parApellidoPaterno.Value = txtApellidoPaterno.Text
-
-        comando.Parameters.Add(parApellidoPaterno)
-
-        Dim parApellidoMaterno As SqlParameter
-        parApellidoMaterno = New SqlParameter
-        parApellidoMaterno.ParameterName = "apellidomaterno"
-        parApellidoMaterno.Value = txtApellidoMaterno.Text
-
-        comando.Parameters.Add(parApellidoMaterno)
-
-        Dim parCarreraId As SqlParameter
-        parCarreraId = New SqlParameter
-        parCarreraId.ParameterName = "carreraid"
-        parCarreraId.Value = txtCarreraId.Text
-
-        comando.Parameters.Add(parCarreraId)
-
-        comando.ExecuteNonQuery()
+        db.Execute("spAlumnoUpdate")
 
         llenaGrid()
     End Sub
