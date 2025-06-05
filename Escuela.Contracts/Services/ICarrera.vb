c3e92d1611fb18c98a7df211b8ba3a94f9fc50a5
Juan Calzoncit <_juan_alejandro@outlook.com>
2025-06-04 19:16:05 -0600

    Add: Creación del proyecto Escuela.Contracts

---
+++ Escuela.Contracts/Services/ICarrera.vb
@@ -0,0 +1,17 @@
+﻿Imports Escuela.Contracts.DataTransferObjects
+
+Namespace Services
+    Public Interface ICarrera
+
+        Sub Insert(ByVal dto As CarreraDTO)
+
+        Sub DeleteById(ByVal carreraid As Integer)
+
+        Sub Update(ByVal dto As CarreraDTO)
+
+        Function GetList() As DataTable
+
+        Function GetById(ByVal carreraid As Integer) As CarreraDTO
+
+    End Interface
+End Namespace
